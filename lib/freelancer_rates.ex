ec7bf9095f46cb8441060a0e77eb846228891031
Nitesh Mishra <>
2023-04-27 19:41:32 +0530

    Add logic to solve problem

--- lib/freelancer_rates.ex
+++ lib/freelancer_rates.ex
@@ -1,17 +1,26 @@
 defmodule FreelancerRates do
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
+    hourly_rate * 8.0
   end
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+    before_discount - before_discount * discount / 100
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    monthly_rate_before_discount = hourly_rate * 8 * 22
+
+    trunc(
+      Float.ceil(monthly_rate_before_discount - monthly_rate_before_discount * discount / 100)
+    )
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    daily_rate_before_discount = hourly_rate * 8
+
+    daily_rate_after_discount =
+      daily_rate_before_discount - daily_rate_before_discount * discount / 100
+
+    Float.floor(budget / daily_rate_after_discount, 1)
   end
 end
